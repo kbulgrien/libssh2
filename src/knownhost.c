kbulgrien/libssh2

commit 8b46528c0476af50a43b6e619f2db318a0cad194

Ben Kibbey fixed the hostline() when parsing lines using only one hostname

src/knownhost.c
@@ -394,6 +394,7 @@ static int hostline(LIBSSH2_KNOWNHOSTS *hosts,
                     const char *key, size_t keylen)
 {
     const char *p;
+    const char *orig = host;
     const char *salt = NULL;
     int rc;
     int type = LIBSSH2_KNOWNHOST_TYPE_PLAIN;
@@ -422,6 +423,8 @@ static int hostline(LIBSSH2_KNOWNHOSTS *hosts,
             hostlen -= scan; /* deduct what's left to scan from the first
                                 host name */
         }
+        else
+            host = orig;
     }
     else {
         /* |1|[salt]|[hash] */
@@ -507,6 +510,7 @@ static int hostline(LIBSSH2_KNOWNHOSTS *hosts,
             return rc;
     }
 
+    host = orig;
     memcpy(hostbuf, host, hostlen);
     hostbuf[hostlen]=0;
 

commit 9bfd3777dfc42e67fbb36c480b259215789b8e38

Fix the functionality again with hashed host names again.
The breakage came with commit 8b46528c0476af50a43b6e619f2db318a0cad194.

src/knownhost.c
@@ -509,7 +509,6 @@ static int hostline(LIBSSH2_KNOWNHOSTS *hosts,
             return rc;
     }
 
-    host = orig;
     memcpy(hostbuf, host, hostlen);
     hostbuf[hostlen]=0;
 